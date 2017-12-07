57ff938153d0bec07d005d5a3bad718a673aded0
PoomariMariappan <33797193+PoomariMariappan@users.noreply.github.com>
2017-12-07 13:45:22 +0530

    Create Convert.java

---
+++ Convert.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Convert
+{
+public static void main(String args[])
+{
+int k;
+String d;
+Scanner s=new Scanner(System.in);
+d=s.nextLine();
+k=Integer.parseInt(d,29);
+System.out.println(k);
+}
+}

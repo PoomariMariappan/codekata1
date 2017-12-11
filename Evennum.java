83b7e9e20fcdc14b10761584c2c5843957e86b10
PoomariMariappan <33797193+PoomariMariappan@users.noreply.github.com>
2017-12-11 11:06:41 +0530

    Create Evennum.java

---
+++ Evennum.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Evennum{
+
+     public static void main(String []args){
+       Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int d=s.nextInt();
+int a,i;
+for(i=n;i<d;i++)
+{
+  if((i%2)==0)
+System.out.println(i);
+}
+ }
+}

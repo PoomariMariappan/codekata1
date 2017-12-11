e1f3f8c75504d0240c7af70e2bbc02deb22da732
PoomariMariappan <33797193+PoomariMariappan@users.noreply.github.com>
2017-12-11 09:51:18 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Alphabet{
+
+     public static void main(String []args){
+       Scanner s=new Scanner(System.in);
+char n;
+int i;
+n=s.next().charAt(0);
+if(Character.isLetter(n))
+System.out.println("alphabet");
+
+ }
+}

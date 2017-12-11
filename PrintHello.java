1bac7c480727b07c7d8ef2bd7bd3a4bb091a05ad
PoomariMariappan <33797193+PoomariMariappan@users.noreply.github.com>
2017-12-11 10:06:11 +0530

    Create  PrintHello.java

---
+++ PrintHello.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class PrintHello{
+
+     public static void main(String []args){
+       Scanner s=new Scanner(System.in);
+String n="Hello";
+int i;
+for(i=0;i<5;i++)
+System.out.println(n);
+ }
+}

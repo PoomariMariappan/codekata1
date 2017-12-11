198de942858e3244e700b5253e61b815720d5ec1
PoomariMariappan <33797193+PoomariMariappan@users.noreply.github.com>
2017-12-11 10:01:52 +0530

    Create Leapyear.java

---
+++ Leapyear.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Leapyear{
+
+     public static void main(String []args){
+       Scanner s=new Scanner(System.in);
+int i,n;
+n=s.nextInt();
+if(n%4==0)
+System.out.println("leap year");
+else
+System.out.println("not");
+ }
+}
